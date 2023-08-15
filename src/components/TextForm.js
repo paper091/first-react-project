33bb6c689b3689f3238a6d073030f986a22c7164
unknown <nitishrawat009@gmail.com>
2023-08-15 06:18:19 +0530

    added whisper, scream, and some other functionaitites

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,14 +17,29 @@ export default function TextForm(props) {
         console.log("Button Clicked" + text);
         let newText = text.toUpperCase();
         setText(newText)
+    }    
+    const handleLowClick = ()=>{
+        console.log("Low click");
+        console.log("Button Clicked" + text);
+        let newText = text.toLowerCase();
+        setText(newText)
     }
     return (
         <>
-            <h1>{props.heading}</h1>
-            <div className="input-group mb-3"> 
-                <textarea className="form-control" id="textInput" value={text} onChange={handleOnChange} aria-label="With textarea" rows="8"></textarea>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="input-group mb-3"> 
+                    <textarea className="form-control" id="textInput" value={text} onChange={handleOnChange} aria-label="With textarea" rows="8"></textarea>
+                </div>
+                <button className="btn btn-primary mx-3" onClick={handleUpClick}>SCREAM!!</button>
+                <button className="btn btn-primary mx-3" onClick={handleLowClick}>whisper</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to upperCase</button>
+
+            <div className="component my-5">
+                <h1>Your text summary</h1>
+                <p>Your text has {text.split(" ").length} words and {text.length} characters.</p>
+                <p>Your text would take {0.0034*text.split(" ").length} minutes.</p>
+            </div>
         </>
     )
 }
